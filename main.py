96ab6a9107f01ea12126c1091c259f254bd9de7a
Majo <mjguirolam@gmail.com>
2026-09-12 23:30:26 -0600

    definir variables y funciones en el programa
    Se agregan las librerías y definimos las varaibles de arrays, boolean y enteros. Configure la primer decisión del código o modo del menu

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from funciones import (
+    mostrar_menu,
+    ingresar_producto,
+    realizar_venta,
+    verificar_inventario
+)
 
+productos = []
+proveedores = []
+precios = []
+cantidades = []
+codigos = []
+siguiente_codigo = 1
+funcionamiento = True
+
+while funcionamiento:
+
+    decision = mostrar_menu()
+
+    if decision == 1:
+
+        siguiente_codigo = ingresar_producto(
+            productos,
+            proveedores,
+            precios,
+            cantidades,
+            codigos,
+            siguiente_codigo
+        )
